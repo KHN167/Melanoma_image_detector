dbe50385bc40792f4374798f121509bf8abe2722
Khn167 <knguyen101@my.bcit.ca>
2023-10-15 19:30:31 -0700

    added verification of images to make sure that uploaded images are valid.

--- Melanoma_detector/Melanoma_detector/views.py
+++ Melanoma_detector/Melanoma_detector/views.py
@@ -6,11 +6,21 @@ from PIL import Image
 import cv2
 import tensorflow as tf
 import numpy as np
+from django.http import HttpResponse
 
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def is_valid_image(uploaded_image):
+    try:
+        img = Image.open(uploaded_image)
+        img.verify()  # Verifies the image data
+        return True
+    except (IOError, SyntaxError) as e:
+        return False
 
 def upload_image(request):
     if request.method == 'POST' and request.FILES['image']:
@@ -20,19 +30,26 @@ def upload_image(request):
         model_path = os.path.join(settings.BASE_DIR, 'benignmalignant.h5')
         model = load_model(model_path)
 
+        img = Image.open(uploaded_image)
+        
 
-        with Image.open(uploaded_image) as img:
-            img1 = np.array(img)
 
-            resize = cv2.resize(img1, (256, 256))
-            resize = tf.image.resize(resize, (256, 256))
+        if is_valid_image(img):
+            with Image.open(uploaded_image) as img:
+                img1 = np.array(img)
 
-            yhat = model.predict(np.expand_dims(resize/255, 0))
+                resize = cv2.resize(img1, (256, 256))
+                resize = tf.image.resize(resize, (256, 256))
 
-            if yhat > 0.5:
-                predicted_class = 'malignant'
-            else:
-                predicted_class = 'benign'
+                yhat = model.predict(np.expand_dims(resize/255, 0))
+
+                if yhat > 0.5:
+                    predicted_class = 'malignant'
+                else:
+                    predicted_class = 'benign'
+
+        else:
+            return HttpResponse('invalid image file. please upload a valid image.')
 
 
 
